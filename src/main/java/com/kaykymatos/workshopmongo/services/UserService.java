6e63e8e987242d467c4fc2c06ef68bcabb81c9cc
Kayky Matos Santana <kayky7277@gmail.com>
2025-03-30 11:35:04 -0300

    Obtendo um usuário por id

--- src/main/java/com/kaykymatos/workshopmongo/services/UserService.java
+++ src/main/java/com/kaykymatos/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.kaykymatos.workshopmongo.services;
 
 import com.kaykymatos.workshopmongo.domain.User;
 import com.kaykymatos.workshopmongo.repository.UserRepository;
+import com.kaykymatos.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,5 +16,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
---
+++ src/main/java/com/kaykymatos/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.kaykymatos.workshopmongo.services.exception;
+
+import java.io.Serial;
+
+public class ObjectNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
