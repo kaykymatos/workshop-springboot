399e57e6c9abe922ed9684a5dc45206b2f7503e2
Kayky Matos Santana <kayky7277@gmail.com>
2025-04-13 14:30:16 -0300

    Consulta simples com @Query

--- src/main/java/com/kaykymatos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/kaykymatos/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,14 @@ package com.kaykymatos.workshopmongo.repository;
 import com.kaykymatos.workshopmongo.domain.Post;
 import com.kaykymatos.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+    @Query("{ title: { $regex: ?0, $options:'i'}}")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
